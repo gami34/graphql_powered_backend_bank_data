4c39e925a3b5dcbd1215af869c14209762a4a0a6
Gideon Mmeremnwanne <gmmeremnwanne@gmail.com>
2021-11-10 15:43:39 +0100

    removed hard coding of host name

--- bin/json-graphql-server.js
+++ bin/json-graphql-server.js
@@ -8,24 +8,12 @@ var JsonGraphqlServer = require('../lib/json-graphql-server.node.min').default;
 var dataFilePath = process.argv.length > 2 ? process.argv[2] : './data.json';
 var data = require(path.join(process.cwd(), dataFilePath));
 var PORT = process.env.PORT || 8080;
-var HOST = process.env.HOST || 'localhost';
 var app = express();
-
-process.argv.forEach((arg, index) => {
-    // allow a custom port via CLI
-    if (arg === '--p' && process.argv.length > index + 1) {
-        PORT = process.argv[index + 1];
-    }
-
-    if (arg === '--h' && process.argv.length > index + 1) {
-        HOST = process.argv[index + 1];
-    }
-});
 
 app.use(cors());
 app.use('/', JsonGraphqlServer(data));
-app.listen(PORT, HOST);
-var msg = `GraphQL server running with your data at http://${HOST}:${PORT}/`;
+app.listen(PORT);
+var msg = `GraphQL server running`;
 console.log(msg); // eslint-disable-line no-console
 
 process.on('unhandledRejection', (reason, p) => {
