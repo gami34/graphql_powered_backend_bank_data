5abdb70a81de2754f3c5c136b6bf152ef4dffff5
Gideon Mmeremnwanne <gmmeremnwanne@gmail.com>
2021-11-10 14:55:20 +0100

    changed data.js to used db.js content

--- bin/json-graphql-server.js
+++ bin/json-graphql-server.js
@@ -7,19 +7,19 @@ var JsonGraphqlServer = require('../lib/json-graphql-server.node.min').default;
 
 var dataFilePath = process.argv.length > 2 ? process.argv[2] : './data.json';
 var data = require(path.join(process.cwd(), dataFilePath));
-var PORT = process.env.NODE_PORT || 3000;
+var PORT = process.env.NODE_PORT || 8080;
 var HOST = process.env.NODE_HOST || 'localhost';
 var app = express();
 
 process.argv.forEach((arg, index) => {
-  // allow a custom port via CLI
-  if (arg === '--p' && process.argv.length > index + 1) {
-    PORT = process.argv[index + 1];
-  }
+    // allow a custom port via CLI
+    if (arg === '--p' && process.argv.length > index + 1) {
+        PORT = process.argv[index + 1];
+    }
 
-  if (arg === '--h' && process.argv.length > index + 1) {
-    HOST = process.argv[index + 1];
-  }
+    if (arg === '--h' && process.argv.length > index + 1) {
+        HOST = process.argv[index + 1];
+    }
 });
 
 app.use(cors());
@@ -29,5 +29,5 @@ var msg = `GraphQL server running with your data at http://${HOST}:${PORT}/`;
 console.log(msg); // eslint-disable-line no-console
 
 process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
